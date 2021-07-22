59bfa04967bc82af5aed56a6921d2eaf6f48452c
Carlos Rosales <carlosros033@gmail.com>
2021-07-22 16:36:15 -0400

    feat(app): Initial Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import sitemap from "constants/sitemap";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path={sitemap.home.path}></Route>
+        <Route path={sitemap.faves.path}></Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/constants/sitemap.ts
@@ -0,0 +1,10 @@
+const sitemap = {
+  home: {
+    path: "/",
+  },
+  faves: {
+    path: "/faves",
+  },
+};
+
+export default sitemap;
