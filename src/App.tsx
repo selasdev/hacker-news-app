a9f6b03ea8e0778532306c4e4029b04c861064a7
Carlos Rosales <carlosros033@gmail.com>
2021-07-22 16:54:18 -0400

    feat(header): Creating Header component
    -Created a Header component to be used as main header of the page
    -removed some unused files
    
    BREAKING CHANGE: added "@types/styled-components" dependency

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import sitemap from "constants/sitemap";
 
+import Header from "components/Header";
+
 const App = () => {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route path={sitemap.home.path}></Route>
         <Route path={sitemap.faves.path}></Route>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainHeader, PageTitle } from "./styles";
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <PageTitle>Hacker News</PageTitle>
+    </MainHeader>
+  );
+};
+
+export default Header;
