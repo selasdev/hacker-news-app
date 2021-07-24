14b7b8f318270ad979f3ffe282290768ced0dc19
Carlos Rosales <carlosros033@gmail.com>
2021-07-24 01:10:46 -0400

    feat: faves page

--- src/containers/Routes.tsx
+++ src/containers/Routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from "components/Navigation";
 import All from "pages/All";
+import Faves from "pages/Faves";
 
 import sitemap from "constants/sitemap";
 
@@ -10,10 +11,12 @@ const Routes = () => {
     <Router>
       <Navigation />
       <Switch>
-        <Route path={sitemap.home.path}>
+        <Route exact path={sitemap.home.path}>
           <All />
         </Route>
-        <Route path={sitemap.faves.path}></Route>
+        <Route exact path={sitemap.faves.path}>
+          <Faves />
+        </Route>
       </Switch>
     </Router>
   );
